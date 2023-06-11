afd121fe94a4792fa8a898ec87eb78e1ed273e8f
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2023-06-11 12:11:41 +0000

    change config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,14 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://IRiide-Next.github.io',
+  url: 'https://iriide-next.github.io',
   baseUrl: '/technical-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'iriide-next', // Usually your GitHub org/user name.
+  projectName: 'technical-docs', // Usually your repo name.
+  trailingSlash: false,
   presets: [
     [
       '@docusaurus/preset-classic',
